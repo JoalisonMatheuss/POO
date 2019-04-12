ce449ccf93dbee9fbf27610f3c8eddc8832c528c
JoalisonMatheuss <44174090+JoalisonMatheuss@users.noreply.github.com>
2019-04-12 11:49:54 -0300

    Funcionario

--- Funcionario.java
+++ Funcionario.java
@@ -26,26 +26,93 @@ class Funcionario{
     //Metodos
 
     public void setExibir(){
-      system.out.println("codigo: " +codigo);
-      system.out.println("nome: " +nome);
-      system.out.println("cpf: " +cpf);
-      system.out.println("endereco: " +endereco);
-      system.out.println("telefone: " +telefone);
-      system.out.println("idade: " +idade);
-      system.out.println("salario: " +salario);
-      system.out.println("num_dependentes: " +num_dependentes);
+      System.out.println("\n"+"   DADOS DO FUNCIONARIO" + "\n");
+      System.out.println("--------------------------------");
+      System.out.println("|"+" codigo --> " + codigo);
+      System.out.println("|"+" nome --> " + nome);
+      System.out.println("|"+" cpf --> " + cpf);
+      System.out.println("|"+" endereco --> " + endereco);
+      System.out.println("|"+" telefone --> " + telefone);
+      System.out.println("|"+" idade --> " + idade);
+      System.out.println("|"+" salario --> " + salario);
+      System.out.println("|"+" num_dependentes --> " + num_dependentes);
+      System.out.println("--------------------------------");
     }
 
-    public void setAlterarSalario(double valor){
+    public void setAlterarSalario(double salario){
       this.salario = salario;
+    }
+
+    public int getCodigo(){
+      return codigo;
+    }
+
+    public void setCodigo(int codigo){
+      this.codigo = codigo;
+    }
+
+    public String getNome(){
+      return nome;
+    }
+
+    public void setNome(String nome){
+      this.nome = nome;
+    }
+
+    public int getCpf(){
+      return cpf;
+    }
+
+    public void setCpf(int cpf){
+      this.cpf = cpf;
+    }
+
+    public String getEndereco(){
+      return endereco;
+    }
+    
+    public void setEndereco(String endereco){
+      this.endereco = endereco;
+    }
+
+    public int getTelefone(){
+      return telefone;
+    }
+
+    public void setTelefone(int telefone){
+      this.telefone = telefone;
+    }
+
+    public int getIdade(){
+      return idade;
+    }
+    
+    public void setIdade(int idade){
+      this.idade = idade;
     }
 
     public double getcalcularSalarioLiquido(int percent){
       return (salario - salario*0.11)+(salario * percent * num_dependentes);
     }
+
+    public void setcalcularSalarioLiquido(double salario){
+      this.salario = salario;
+    }
+
+    public int getNum_dependentes(){
+      return num_dependentes;
+    }
+
+    public void setNum_dependentes(int num_dependentes){
+      this.num_dependentes = num_dependentes;
+    }
+
 }
+
+
 public class Main{
   public static void main(String[] args){
-    Funcionario fun = new Funcionario()
+    Funcionario funcionario = new Funcionario(1, "Jooj", 1234567891, "RUA I DON'T NOW", 999799532, 25, 998.00, 0);
+    funcionario.setExibir();
   }
 }
