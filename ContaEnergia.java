c36d381564d6ec3fd135c923a97bdc6401cb3870
JoalisonMatheuss <44174090+JoalisonMatheuss@users.noreply.github.com>
2019-04-24 18:22:50 -0300

    Conta de Energia

--- ContaEnergia.java
+++ ContaEnergia.java
@@ -1,10 +1,10 @@
 class ContaEnergia{
   //  Atributos
     private String nome;
-    private double consumo;
+    private int consumo;
 
     // Construtor
-    public ContaEnergia(String nome, double consumo){
+    public ContaEnergia(String nome, int consumo){
       this.nome = nome;
       this.consumo = consumo;
     }
@@ -13,24 +13,30 @@ class ContaEnergia{
       System.out.println("Nome do Cliente: "+nome);
       System.out.println("Consumo: "+consumo);
     }
-    
     public String getNome(){
       return nome;
     }
     public void setNome(String nome){
       this.nome = nome;
     }
-    public double getConsumo(){
+    public int getConsumo(){
       return consumo;
     }
-    public void setConsumo(double consumo){
+    public void setConsumo(int consumo){
       this.consumo = consumo;
+    }
+    public void setCalcuclarConsumo(){
+      if (consumo == 50){
+        System.out.println("R$ "+10.00);
+      } else if(consumo > 50){
+        System.out.println(consumo*0.20);
+      }
     }
 }
 
 public class Main{
   public static void main(String[] args){
-    ContaEnergia conta = new ContaEnergia("Joalison", 10.00);
+    ContaEnergia conta = new ContaEnergia("Joalison", 50);
     conta.setExibir();
   }
 }
