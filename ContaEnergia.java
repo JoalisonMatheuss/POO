c95f1ed235356e392816d20d35d2126d98feace1
JoalisonMatheuss <44174090+JoalisonMatheuss@users.noreply.github.com>
2019-04-25 11:56:22 -0300

    Conta de Energia

--- ContaEnergia.java
+++ ContaEnergia.java
@@ -8,7 +8,7 @@ class ContaEnergia{
       this.nome = nome;
       this.consumo = consumo;
     }
-    public void setExibir(){
+    public void Exibir(){
       System.out.println("----- Dados do Consumidor -----");
       System.out.println("Nome do Cliente: "+nome);
       System.out.println("Consumo: "+consumo);
@@ -25,20 +25,23 @@ class ContaEnergia{
     public void setConsumo(int consumo){
       this.consumo = consumo;
     }
-    public void setCalcuclarConsumo(){
-      if (consumo == 50){
+    public void CalcuclarConsumo(){
+      if (consumo <= 50){
         System.out.println(10.00);
       } else if(consumo <= 100){
-        System.out.println((10+consumo-50)*0.20);
-        } else{
-          System.out.println((10+consumo-50)*0.35);
-      }
+        System.out.println(10+(consumo-50)*0.20);
+        } else if(consumo <=200){
+          System.out.println((10+(50* 0.20) + (consumo - 100) * 0.25));
+      } else{
+        System.out.println(10+(50* 0.20) + (consumo - 100) * 0.25 + (consumo -200)*0.35);
+       }
     }
 }
 
 public class Main{
   public static void main(String[] args){
-    ContaEnergia conta = new ContaEnergia("Joalison", 50);
-    conta.setExibir();
+    ContaEnergia conta = new ContaEnergia("Joalison", 200);
+    conta.Exibir();
+    conta.CalcuclarConsumo();
   }
 }
